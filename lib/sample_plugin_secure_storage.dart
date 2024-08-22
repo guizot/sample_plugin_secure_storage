4fe139d6fdb75206c211d0194fabf98449381e26
autentika <rinaldi@autentika.id>
2024-08-22 14:34:36 +0700

    update file

--- lib/sample_plugin_secure_storage.dart
+++ lib/sample_plugin_secure_storage.dart
@@ -1,12 +1,13 @@
+import 'package:flutter/material.dart';
 import 'sample_plugin_secure_storage_platform_interface.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SamplePluginSecureStorage {
-  Future<String?> getPlatformVersion() async{
+  Future<String?> getPlatformVersion() async {
     const storage = FlutterSecureStorage();
     await storage.write(key: "encrypted_key", value: "1234567890");
     String? value = await storage.read(key: "encrypted_key");
-    return value;
-    // return SamplePluginSecureStoragePlatform.instance.getPlatformVersion();
+    debugPrint("encrypted_key: $value");
+    return SamplePluginSecureStoragePlatform.instance.getPlatformVersion();
   }
 }
